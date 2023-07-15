d97db447d0a21f8af82949cb849caf8cfa736ed8
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-07-16 01:41:19 +0800

    Finally able to connect to database

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const{getDb, connectToDb} = require('./db')
 
 // init app & middleware
 const server = express()
 
-server.listen(3000, () => {
-  console.log('server listening on port 3000')
+
+// db connection
+let db
+
+connectToDb((err) => {
+  if(!err){
+    server.listen(3000, () => {
+      console.log('server listening on port 3000')
+    })
+    db = getDb()
+  }
 })
 
 // routes

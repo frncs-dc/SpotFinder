71ad8d1907a72394824accfe0b06eda6824617ea
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-07-22 12:37:22 +0800

    Some minor fixes/removals across the whole proect

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -30,193 +30,6 @@ const authMiddleware = (req, res, next ) => {
   }
 }
 
-
-/**
- * GET /
- * Admin - Login Page
-*/
-router.get('/admin', async (req, res) => {
-  try {
-    const locals = {
-      title: "Admin",
-      description: "Simple Blog created with NodeJs, Express & MongoDb."
-    }
-
-    res.render('admin/index', { locals, layout: adminLayout });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-
-/**
- * POST /
- * Admin - Check Login
-*/
-router.post('/admin', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    
-    const user = await User.findOne( { username } );
-
-    if(!user) {
-      return res.status(401).json( { message: 'Invalid credentials' } );
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordValid) {
-      return res.status(401).json( { message: 'Invalid credentials' } );
-    }
-
-    const token = jwt.sign({ userId: user._id}, jwtSecret );
-    res.cookie('token', token, { httpOnly: true });
-    res.redirect('/dashboard');
-
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-
-/**
- * GET /
- * Admin Dashboard
-*/
-// router.get('/dashboard', authMiddleware, async (req, res) => {
-//   try {
-//     const locals = {
-//       title: 'Dashboard',
-//       description: 'Simple Blog created with NodeJs, Express & MongoDb.'
-//     }
-
-//     const data = await Post.find();
-//     res.render('admin/dashboard', {
-//       locals,
-//       data,
-//       layout: adminLayout
-//     });
-
-//   } catch (error) {
-//     console.log(error);
-//   }
-
-// });
-
-
-/**
- * GET /
- * Admin - Create New Post
-*/
-router.get('/add-post', authMiddleware, async (req, res) => {
-  try {
-    const locals = {
-      title: 'Add Post',
-      description: 'Simple Blog created with NodeJs, Express & MongoDb.'
-    }
-
-    const data = await Post.find();
-    res.render('admin/add-post', {
-      locals,
-      layout: adminLayout
-    });
-
-  } catch (error) {
-    console.log(error);
-  }
-
-});
-
-
-/**
- * POST /
- * Admin - Create New Post
-*/
-router.post('/add-post', authMiddleware, async (req, res) => {
-  try {
-    try {
-      const newPost = new Post({
-        title: req.body.title,
-        body: req.body.body
-      });
-
-      await Post.create(newPost);
-      res.redirect('/dashboard');
-    } catch (error) {
-      console.log(error);
-    }
-
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-
-/**
- * GET /
- * Admin - Create New Post
-*/
-router.get('/edit-post/:id', authMiddleware, async (req, res) => {
-  try {
-
-    const locals = {
-      title: "Edit Post",
-      description: "Free NodeJs User Management System",
-    };
-
-    const data = await Post.findOne({ _id: req.params.id });
-
-    res.render('admin/edit-post', {
-      locals,
-      data,
-      layout: adminLayout
-    })
-
-  } catch (error) {
-    console.log(error);
-  }
-
-});
-
-
-/**
- * PUT /
- * Admin - Create New Post
-*/
-router.put('/edit-post/:id', authMiddleware, async (req, res) => {
-  try {
-
-    await Post.findByIdAndUpdate(req.params.id, {
-      title: req.body.title,
-      body: req.body.body,
-      updatedAt: Date.now()
-    });
-
-    res.redirect(`/edit-post/${req.params.id}`);
-
-  } catch (error) {
-    console.log(error);
-  }
-
-});
-
-
-// router.post('/admin', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-    
-//     if(req.body.username === 'admin' && req.body.password === 'password') {
-//       res.send('You are logged in.')
-//     } else {
-//       res.send('Wrong username or password');
-//     }
-
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-
 /**
  * POST /
  * Admin - Register
@@ -267,6 +80,28 @@ router.put('/edit-post/:id', authMiddleware, async (req, res) => {
     }
   });
 
+
+  router.post('/Profile', async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      try {
+        const user = await User.create({ username, email, password:hashedPassword});
+        res.redirect('/park');
+      } catch (error) {
+        if(error.code === 11000) {
+          res.redirect('/LogInPage')
+        }
+       
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
   router.get('/park', async (req, res) => {
     res.render('park',{
         current_user: current_user
@@ -276,7 +111,7 @@ router.put('/edit-post/:id', authMiddleware, async (req, res) => {
   router.get('/Profile', async (req, res) => {
     res.render('Profile', {
       current_user: current_user
-    })
+    });
   });
 /**
  * DELETE /
